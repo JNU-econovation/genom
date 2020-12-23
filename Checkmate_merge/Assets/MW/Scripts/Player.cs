a381b592a4f9a6dc0911afd5e83825d7f8910cf5
coolblue25721853 <coolblue1853@gmail.com>
2020-12-24 01:39:35 +0900

    새일러스트 삽입

--- Checkmate_merge/Assets/MW/Scripts/Player.cs
+++ Checkmate_merge/Assets/MW/Scripts/Player.cs
@@ -7,11 +7,13 @@ public class Player : Character
     [SerializeField] GameObject player;
     [SerializeField] GameObject playerControllUi;
     private Rigidbody2D playerRigidbody;
+    public int sortingOrder = 0;
+    private SpriteRenderer sprite;
     //[SerializeField] PonController KingController; 
-   
+
     void Start()
     {
-
+        sprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
 
         playerRigidbody = GetComponent<Rigidbody2D>();
@@ -20,7 +22,39 @@ public class Player : Character
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.y > 3.744f && player.transform.position.y < 4.85f)
+        {
+            sprite.sortingOrder = 1;
+        }
+        if (player.transform.position.y > 2.656f && player.transform.position.y < 3.744f)
+        {
+            sprite.sortingOrder = 2;
+        }
+        if (player.transform.position.y > 1.55f && player.transform.position.y < 2.656f)
+        {
+            sprite.sortingOrder = 3;
+        }
+        if (player.transform.position.y > 0.458f && player.transform.position.y < 1.55f)
+        {
+            sprite.sortingOrder = 4;
+        }
+        if (player.transform.position.y > -0.655f && player.transform.position.y < 0.458f)
+        {
+            sprite.sortingOrder = 5;
+        }
+        if (player.transform.position.y > -1.75f && player.transform.position.y < -0.655f)
+        {
+            sprite.sortingOrder = 6;
+        }
+
+        if (player.transform.position.y > -2.84f && player.transform.position.y < -1.75f)
+        {
+            sprite.sortingOrder = 7;
+        }
+        if (player.transform.position.y > -3.94f && player.transform.position.y < -2.84f)
+        {
+            sprite.sortingOrder = 8;
+        }
     }
 
     public void PlayerKilledByEnemy()
