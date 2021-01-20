16edb8bca2543d9582bf301ae22c6655c26ae391
coolblue25721853 <coolblue1853@gmail.com>
2021-01-20 14:35:44 +0900

    스킬 쿨타임 개선

--- Checkmate_merge/Assets/SpeacilATT.cs
+++ Checkmate_merge/Assets/SpeacilATT.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-
+using UnityEngine.UI;
 public class SpeacilATT : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    float delayTime = 6600;
+    public Image image;
     public Animator animator;
     Vector2 devilHandPos = new Vector2(-18.25f, -1.46f);
     public GameObject skillATT;
@@ -22,19 +24,38 @@ public class SpeacilATT : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
 
     }
+    IEnumerator Wait()
+    {
 
+        image.fillAmount = 0;
+        while (delayTime > 0)
+        {
 
+            image.fillAmount += (0.00015151515151515151515151515151515f);
+            delayTime -= 1;
+            yield return new WaitForSeconds(0.01f);
+        }
+        delayTime = 6600;
+
+    }
+
+    float cooldown = 66f;
 
     // Update is called once per frame
     void Update()
     {
+        if (isCoolTime == true)
+        {
+            image.fillAmount += 1 / cooldown * Time.deltaTime;
+        }
 
         if (Input.GetKeyDown(KeyCode.X))
         {
             if (isCoolTime == false)
             {
+                image.fillAmount = 0;
                 Instantiate(skillATT, devilHandPos, Quaternion.identity);
-                animator.SetFloat("MAG", 1);
+                //StartCoroutine(Wait());
                 isCoolTime = true;
                 StartCoroutine(coolTime());
             }
@@ -55,8 +76,9 @@ public class SpeacilATT : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         if (isCoolTime == false)
         {
+            image.fillAmount = 0;
             Instantiate(skillATT, devilHandPos, Quaternion.identity);
-            animator.SetFloat("MAG", 1);
+            //StartCoroutine(Wait());
             isCoolTime = true;
             StartCoroutine(coolTime());
         }
@@ -68,7 +90,7 @@ public class SpeacilATT : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     IEnumerator coolTime()
     {
         yield return new WaitForSeconds(66f);
-        animator.SetFloat("MAG", 0);
+
         isCoolTime = false;
     }
 }
