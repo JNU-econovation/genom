1410516f5366a8f301fd9c63d16183ec043f053f
coolblue25721853 <coolblue1853@gmail.com>
2021-01-23 11:40:55 +0900

    인트로 씬 추가, 기타수정사항

--- Checkmate_merge/Assets/JH/Script/MenuControl.cs
+++ Checkmate_merge/Assets/JH/Script/MenuControl.cs
@@ -13,8 +13,22 @@ public class MenuControl : MonoBehaviour//메인 메뉴
     }
     public void OnclickStartImortal()//이모탈 버튼을 누르면 무적 시작
     {
+        StartCoroutine(wait());
+
+    }
+    public void OnclickStartIntro()//인트로 -> 메뉴
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1.0f;
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("Game_Immortal");
         Time.timeScale = 1.0f;
+
+
     }
     public void OnclickQuitBtn()//게임종료 버튼을 누르면 게임 종료
     {
